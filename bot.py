a7175caaa8700a212af4c4c0b1eeb47247a4655e
KRrai <60105362+KRrain@users.noreply.github.com>
2025-12-13 20:41:35 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 from ac.decline import setup_decline_command
 from ac.review import setup_review_command
 from vtcs.vtc import setup_vtc_command
-from vtcs.myvtc import setup_myvtc_commands
+from vtcs.myvtc import setup_myvtc_command
 
 # ---------------- CONFIG ----------------
 
